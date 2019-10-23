07d84f5b727fbc66b6cec16c0872122c1b26201a
atm-greg <gregory.blemand@atm-consulting.fr>
2019-10-23 16:33:54 +0200

    add menu

--- core/modules/modprocessRules.class.php
+++ core/modules/modprocessRules.class.php
@@ -254,6 +254,52 @@ class modprocessRules extends DolibarrModules
 		$this->menu = array();			// List of menus to add
 		$r=0;
 
+		$this->menu[$r]=array(	'fk_menu'=>'fk_mainmenu=products',		    // Use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
+			'type'=>'left',			                // This is a Left menu entry
+			'titre'=>'TopMenuprocessRules',
+			'mainmenu'=>'products',
+			'leftmenu'=>'processrules',
+			'url'=>'/processrules/processrules_list.php',
+			'langs'=>'processrules@processrules',	        // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
+			'position'=>100,
+			'enabled'=>'$conf->processrules->enabled',  // Define condition to show or hide menu entry. Use '$conf->nomenclature->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
+			'perms'=>'$user->rights->processrules->read',			                // Use 'perms'=>'$user->rights->nomenclature->level1->level2' if you want your menu with a permission rules
+			'target'=>'',
+			'user'=>0);				                // 0=Menu for internal users, 1=external users, 2=both
+		$r++;
+
+		$this->menu[$r]=array(
+			'fk_menu'=>'fk_mainmenu=products,fk_leftmenu=processrules',		    // Use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
+			'type'=>'left',			                // This is a Left menu entry
+			'titre'=>$langs->trans('NewprocessRules'),
+			'mainmenu'=>'products',
+			'leftmenu'=>'processrules_left_create',
+			'url'=>'/processrules/processrules_card.php?action=create',
+			'langs'=>'processrules@processrules',	        // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
+			'position'=>100+$r,
+			'enabled'=> '$conf->processrules->enabled',  // Define condition to show or hide menu entry. Use '$conf->missionorder->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
+			'perms'=> '$user->rights->processrules->write',			                // Use 'perms'=>'$user->rights->missionorder->level1->level2' if you want your menu with a permission rules
+			'target'=>'',
+			'user'=>0
+		);				                // 0=Menu for internal users, 1=external users, 2=both
+		$r++;
+
+		$this->menu[$r]=array(
+			'fk_menu'=>'fk_mainmenu=products,fk_leftmenu=processrules',		    // Use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
+			'type'=>'left',			                // This is a Left menu entry
+			'titre'=>$langs->trans('List'),
+			'mainmenu'=>'products',
+			'leftmenu'=>'processrules_left_list',
+			'url'=>'/processrules/processrules_list.php',
+			'langs'=>'processrules@processrules',	        // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
+			'position'=>100+$r,
+			'enabled'=> '$conf->processrules->enabled',  // Define condition to show or hide menu entry. Use '$conf->missionorder->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
+			'perms'=> '$user->rights->processrules->read',			                // Use 'perms'=>'$user->rights->missionorder->level1->level2' if you want your menu with a permission rules
+			'target'=>'',
+			'user'=>0
+		);				                // 0=Menu for internal users, 1=external users, 2=both
+		$r++;
+
 		// Add here entries to declare new menus
 		//
 		// Example to declare a new Top Menu entry and its Left menu entry:
--- processrules_list.php
+++ processrules_list.php
@@ -17,6 +17,7 @@
 
 require 'config.php';
 dol_include_once('processrules/class/processrules.class.php');
+require_once DOL_DOCUMENT_ROOT.'/core/class/html.form.class.php';
 
 if(empty($user->rights->processrules->read)) accessforbidden();
 
@@ -27,6 +28,7 @@ $langs->load('processrules@processrules');
 $massaction = GETPOST('massaction', 'alpha');
 $confirmmassaction = GETPOST('confirmmassaction', 'alpha');
 $toselect = GETPOST('toselect', 'array');
+$search_product = GETPOST('Listview_processrules_search_fk_product');
 
 $object = new processRules($db);
 
@@ -51,10 +53,14 @@ if (!GETPOST('confirmmassaction', 'alpha') && $massaction != 'presend' && $massa
     $massaction = '';
 }
 
+if (GETPOST('button_removefilter_x') == 'x')
+{
+	$search_product = '';
+}
 
 if (empty($reshook))
 {
-	// do action from GETPOST ... 
+	// do action from GETPOST ...
 }
 
 
@@ -102,7 +108,10 @@ $reshook=$hookmanager->executeHooks('printFieldListWhere', $parameters, $object)
 $sql.=$hookmanager->resPrint;
 
 $formcore = new TFormCore($_SERVER['PHP_SELF'], 'form_list_processrules', 'GET');
-
+$form = new Form($db);
+ob_start();
+$form->select_produits($search_product, 'Listview_processrules_search_fk_product');
+$formproduct = ob_get_clean();
 $nbLine = !empty($user->conf->MAIN_SIZE_LISTE_LIMIT) ? $user->conf->MAIN_SIZE_LISTE_LIMIT : $conf->global->MAIN_SIZE_LISTE_LIMIT;
 
 $r = new Listview($db, 'processrules');
@@ -120,8 +129,9 @@ echo $r->render($sql, array(
         ,'noheader' => 0
         ,'messageNothing' => $langs->trans('NoprocessRules')
         ,'picto_search' => img_picto('', 'search.png', '', 0)
+        ,'morehtmlrighttitle' => dolGetButtonTitle($langs->trans('NewprocessRules'), '', 'fa fa-plus-circle', dol_buildpath('/processrules/processrules_card.php?action=create', 2))
         ,'massactions'=>array(
-            'yourmassactioncode'  => $langs->trans('YourMassActionLabel')
+            //'yourmassactioncode'  => $langs->trans('YourMassActionLabel')
         )
     )
 	,'subQuery' => array()
@@ -133,6 +143,7 @@ echo $r->render($sql, array(
 	,'search' => array(
 		'date_creation' => array('search_type' => 'calendars', 'allow_is_null' => true)
 		,'tms' => array('search_type' => 'calendars', 'allow_is_null' => false)
+		,'fk_product'=>array('search_type' => 'override', 'override' => $formproduct)
 		,'ref' => array('search_type' => true, 'table' => 't', 'field' => 'ref')
 		,'label' => array('search_type' => true, 'table' => array('t', 't'), 'field' => array('label')) // input text de recherche sur plusieurs champs
 		,'status' => array('search_type' => processRules::$TStatus, 'to_translate' => true) // select html, la clé = le status de l'objet, 'to_translate' à true si nécessaire
@@ -143,13 +154,15 @@ echo $r->render($sql, array(
 	)
 	,'title'=>array(
 		'ref' => $langs->trans('Ref.')
+		,'fk_product' => $langs->trans('Product')
 		,'label' => $langs->trans('Label')
 		,'date_creation' => $langs->trans('DateCre')
-		,'tms' => $langs->trans('DateMaj')
+		//,'tms' => $langs->trans('DateMaj')
 
 	)
 	,'eval'=>array(
 		'ref' => '_getObjectNomUrl(\'@rowid@\', \'@val@\')'
+		,'fk_product' => '_getProductNomUrl(\'@val@\')'
 //		,'fk_user' => '_getUserNomUrl(@val@)' // Si on a un fk_user dans notre requête
 	)
 ));
@@ -194,4 +207,21 @@ function _getUserNomUrl($fk_user)
 	}
 
 	return '';
+}
+
+function _getProductNomUrl($fk_product)
+{
+	global $db;
+
+	$ret = '';
+	if (!empty($fk_product))
+	{
+		require_once DOL_DOCUMENT_ROOT."/product/class/product.class.php";
+
+		$p = new Product($db);
+		$res = $p->fetch($fk_product);
+		if ($res > 0) $ret = $p->getNomUrl(1);
+	}
+
+	return $ret;
 }
