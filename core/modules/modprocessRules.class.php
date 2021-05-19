e41d3a1674a0c68d41429bab318f1c55b085ee1f
ATM john <john.botella@atm-consulting.fr>
2021-05-19 14:42:34 +0200

    on n'a rien vu....

--- core/modules/modprocessRules.class.php
+++ core/modules/modprocessRules.class.php
@@ -261,7 +261,7 @@ class modprocessRules extends DolibarrModules
 		$r++;
 
 		$this->rights[$r][0] = $this->numero . $r;	// Permission id (must not be already used)
-		$this->rights[$r][1] = processrules_write';	// Permission label
+		$this->rights[$r][1] = 'processrules_write';	// Permission label
 		$this->rights[$r][3] = 0; 					// Permission by default for new user (0/1)
 		$this->rights[$r][4] = 'write';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$this->rights[$r][5] = '';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
