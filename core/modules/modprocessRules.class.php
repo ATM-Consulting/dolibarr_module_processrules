e8e8f95a31bc1585ff90bba5cdc37dfc37afc632
atm-gregr <greg.rastklan@atm-consulting.fr>
2021-06-30 17:06:46 +0200

    badgeMethod

--- core/modules/modprocessRules.class.php
+++ core/modules/modprocessRules.class.php
@@ -97,10 +97,11 @@ class modprocessRules extends DolibarrModules
 			),
 			// Set here all hooks context managed by module. To find available hook context, make a "grep -r '>initHooks(' *" on source code. You can also set hook context to 'all'
 			'hooks' => array(
-				//   'data' => array(
+				   'data' => array(
+					   'processruleslist'
 				//       'hookcontext1',
 				//       'hookcontext2',
-				//   ),
+				   ),
 				//   'entity' => '0',
 			),
 			// Set this to 1 if features of module are opened to external users
